019182dd56e3dd0a4ed1fd64b60b71c7a4be8b80
Victor Ortega <victordanielortega1@gmail.com>
2026-04-22 15:07:22 -0400

    Elimino

--- inventario juegos/user_input_files/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Configuración de credenciales del administrador
-// NOTA: En un sistema real, estas credenciales estarían hasheadas
-// y almacenadas en una base de datos segura.
-define('ADMIN_EMAIL', 'admin@gmail.com');
-define('ADMIN_PASSWORD', 'contraseña1');
-?>
